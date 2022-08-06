0ab1feb9f2d9080de3c8a068ad55ef0f952940ab
Nishiura <46619833+west-urad24@users.noreply.github.com>
2022-08-06 17:30:28 +0900

    Update AI_app.py

--- AI_app.py
+++ AI_app.py
@@ -6,7 +6,19 @@ import sklearn.datasets
 import sklearn.svm
 import numpy as np
 
-def imageToData(filename):
+# digits.images
+# [[ 0.  0.  1. 14. 11.  0.  0.  0.]
+#  [ 0.  0.  9. 15.  2.  0.  4.  0.]
+#  [ 0.  2. 16.  6.  0.  7. 16.  2.]
+#  [ 0.  8. 16.  6.  6. 16. 12.  0.]
+#  [ 0.  5. 16. 16. 16. 15.  3.  0.]
+#  [ 0.  0.  1.  4. 16.  8.  0.  0.]
+#  [ 0.  0.  0.  9. 16.  1.  0.  0.]
+#  [ 0.  0.  0. 15. 16.  0.  0.  0.]]
+#digits.target
+# 4
+
+def imageToData(filename):#画像を数値に変換
     grayImage = PIL.Image.open(filename).convert("L")#白黒にする
     grayImage = grayImage.resize((8, 8), PIL.Image.ANTIALIAS)
 
@@ -27,13 +39,11 @@ def predictDigits(data):
     n = clf.predict([data])#予測
     textLabel.configure(text = '予測結果は'+str(n)+'です。')
 
-
 def openfile():
     fpath = fd.askopenfilename()#手書きの画像を読み込む
     if fpath:
         data = imageToData(fpath)
         predictDigits(data)#predictDigits関数に移動
-
 
 root = tk.Tk()
 root.geometry("400x400")
